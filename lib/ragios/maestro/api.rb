1b21367e510a198ef13840b60b80c3ec23e5d9d7
Rainmaker <obioraakubue@yahoo.com>
2018-02-24 17:38:54 -0500

    fix error with maestro test api

--- lib/ragios/maestro/api.rb
+++ lib/ragios/maestro/api.rb
@@ -2,7 +2,7 @@ module Ragios
   module Maestro
     class Api
       def self.test(monitor)
-        u = Ragios::Plugin::UptimeMonitor.new
+        u = Ragios::Plugins::UptimeMonitor.new
         u.init(monitor)
         u.test_command?
         u.test_result
